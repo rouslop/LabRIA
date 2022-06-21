0cf1594ba4de02a32f3f048b6b214c91c24bfd56
Lolellus <81826356+Lolellus@users.noreply.github.com>
2022-06-21 19:31:29 -0300

    prueva

--- LabRia/src/app/app-routing.module.ts
+++ LabRia/src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent} from './home/home.component'
 import { MateriaComponent} from './materia/materia.component'
+import { AltaMateriaComponent} from './alta-materia/alta-materia.component'
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'materia', component: MateriaComponent },
-
+  { path: 'altaMateria', component: AltaMateriaComponent },
 ];
 
 @NgModule({
