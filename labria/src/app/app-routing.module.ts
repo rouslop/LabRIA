350223c3a9eaed8a3ad1419bffcb9459ff63f63c
Lolellus <81826356+Lolellus@users.noreply.github.com>
2022-06-25 00:35:58 -0300

    menu mas lindo

--- labria/src/app/app-routing.module.ts
+++ labria/src/app/app-routing.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent} from './home/home.component'
-import { MateriaComponent} from './materia/materia.component'
-import { AltaMateriaComponent} from './alta-materia/alta-materia.component'
-import { NoticiaComponent } from './noticia/noticia.component';
+import { HomeComponent} from './components/home/home.component'
+import { MateriaComponent} from './components/materia/materia.component'
+import { AltaMateriaComponent} from './components/alta-materia/alta-materia.component'
+import { NoticiaComponent } from './components/noticia/noticia.component';
+import { LoginComponent } from './components/login/login.component'
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'materia', component: MateriaComponent },
   { path: 'altaMateria', component: AltaMateriaComponent },
   { path: 'noticia', component: NoticiaComponent},
+  {path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
--- labria/src/app/app.module.ts
+++ labria/src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { SidebarModule } from 'ng-sidebar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { MateriaComponent } from './materia/materia.component';
-import { AltaMateriaComponent } from './alta-materia/alta-materia.component';
-import { ModificarMateriaComponent } from './modificar-materia/modificar-materia.component';
-import { NoticiaComponent } from './noticia/noticia.component';
-import { ModificarNoticiaComponent } from './modificar-noticia/modificar-noticia.component';
-import { AltaNoticiaComponent } from './alta-noticia/alta-noticia.component';
+import { HomeComponent } from './components/home/home.component';
+import { MateriaComponent } from './components/materia/materia.component';
+import { AltaMateriaComponent } from './components/alta-materia/alta-materia.component';
+import { ModificarMateriaComponent } from './components/modificar-materia/modificar-materia.component';
+import { NoticiaComponent } from './components/noticia/noticia.component';
+import { ModificarNoticiaComponent } from './components/modificar-noticia/modificar-noticia.component';
+import { AltaNoticiaComponent } from './components/alta-noticia/alta-noticia.component';
+import { LoginComponent } from './components/login/login.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {NgbPaginationModule, NgbAlertModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -20,11 +24,15 @@ import { AltaNoticiaComponent } from './alta-noticia/alta-noticia.component';
     ModificarMateriaComponent,
     NoticiaComponent,
     ModificarNoticiaComponent,
-    AltaNoticiaComponent
+    AltaNoticiaComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    NgbPaginationModule, 
+    NgbAlertModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
