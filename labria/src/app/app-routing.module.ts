8cf88c5a2cb804edfbf25eea9332b6c154bb3b2e
Lolellus <81826356+Lolellus@users.noreply.github.com>
2022-06-29 21:05:57 -0300

    oden carpetas

--- labria/src/app/app-routing.module.ts
+++ labria/src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent} from './components/home/home.component'
-import { MateriaComponent} from './components/materia/materia.component'
-import { AltaMateriaComponent} from './components/alta-materia/alta-materia.component'
-import { NoticiaComponent } from './components/noticia/noticia.component';
+import { MateriaComponent} from './components/ComponentesMateria/materia/materia.component'
+import { AltaMateriaComponent} from './components/ComponentesMateria/alta-materia/alta-materia.component'
+import { NoticiaComponent } from './components/ComponentesNoticia/noticia/noticia.component';
 import { LoginComponent } from './components/login/login.component'
-import { AltaNoticiaComponent } from './components/alta-noticia/alta-noticia.component';
+import { AltaNoticiaComponent } from './components/ComponentesNoticia/alta-noticia/alta-noticia.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
