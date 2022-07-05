67161f0de34ab69dfb4c545e15e58460dd4ab95e
Romina <romina.lopez@estudiantes.utec.edu.uy>
2022-07-05 15:00:01 -0300

    sigue sin traer los documentos

--- LabRia/src/app/models/documento.ts
+++ LabRia/src/app/models/documento.ts
@@ -2,6 +2,7 @@ export class Documento{
     id: number | undefined;
     titulo: string | undefined;
     tipo: string | undefined;
-    activo: boolean | undefined;
     documentoPDF: string | undefined;
+    activo: boolean | undefined;
+    
 }
