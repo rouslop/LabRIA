219c77c6da400c034a35a1e9c6e2bb09e2a149a4
Lolellus <81826356+Lolellus@users.noreply.github.com>
2022-07-07 09:45:21 -0300

    quedo filtro por semestre

--- LabRia/src/app/components/ComponentesUnidades/unidades/unidades.component.ts
+++ LabRia/src/app/components/ComponentesUnidades/unidades/unidades.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import {Unidades} from 'src/app/models/unidades';
 import { UnidadesService } from 'src/app/services/unidades.service';
@@ -10,9 +11,14 @@ import Swal from 'sweetalert2';
   styleUrls: ['./unidades.component.css']
 })
 export class UnidadesComponent implements OnInit {
-
+  
+  Lunidades: Unidades[] = [];
   unidades: Unidades[] = [];
   token = localStorage.getItem('token');
+
+  formUnidades = new FormGroup ({
+    semestre: new FormControl('', [Validators.required,Validators.min(1),Validators.max(6)]),
+    });
 
   constructor(public UnidadesSvc:UnidadesService,private router:Router) { }
 
@@ -30,7 +36,7 @@ export class UnidadesComponent implements OnInit {
   }
   getListUnidades() {
     this.UnidadesSvc.getUnidades().subscribe({
-      next: value => this.unidades = value,
+      next: value => {this.unidades = value,this.Lunidades=value},
       error: err => { this.alert('Error al cargar las materias: ' ) }
     }
     );
@@ -39,6 +45,16 @@ export class UnidadesComponent implements OnInit {
   agregarPrevia(x: any){
     this.UnidadesSvc.guardarUnidad(x);
     this.router.navigate(['/agregarPrevia']);
+  }
+
+  selecsionarSemestre(){
+    let p: Unidades[] = [];
+    for (let i = 0; i < this.Lunidades.length; i++) {
+      const e: Unidades = this.Lunidades[i];
+      if(e.semestre ==  this.formUnidades.controls["semestre"].value){
+        p.push(e);
+      } }
+      this.unidades=p;   
   }
 
   alert(x:string):void{
